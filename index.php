08a607943ac786993470192973c216efd18cf03a
Ko Phyo Wai Heim <79593958+Omega-twig@users.noreply.github.com>
2023-05-23 14:48:44 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php include"connect.php"
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1 class='primary'>hello</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="card-title">Deta List
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <a href="creat.php" class='btn btn-primary float-end me-3'>+ Add new</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Title</th>
+                                    <th>Description</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $query = "SELECT * FROM posts";
+                                $result = mysqli_query($db,$query);
+                                foreach($result as $row){
+                                ?>
+                                <tr>
+                                    <td><?php echo $row["ID"]; ?></td>
+                                    <td><?php echo $row["title"]; ?></td>
+                                    <td><?php echo $row["description"]; ?></td>
+                                    <td>
+                                        <a href="edit.php?postId=<?php echo $row["ID"]; ?>">Edit</a>
+                                        <a href="">Delete</a>
+                                    </td>
+
+                                </tr>
+                                <?php
+                                }?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
